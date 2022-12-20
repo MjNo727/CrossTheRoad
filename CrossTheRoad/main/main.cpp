7e5f753e827ecebacaf8003449ccebe5bb85c1dc
Milo <72milo27@gmail.com>
2022-12-20 22:54:38 +0700

    Update main.cpp

--- CrossTheRoad/main/main.cpp
+++ CrossTheRoad/main/main.cpp
@@ -3,12 +3,16 @@
 
 using namespace std;
 
+
 int main() {
 	Game game;
+	int temp;
 	
-	game.ResizeConsole(1280, 720);
+	game.ResizeConsole(1366, 768);
 	game.FixConsoleWindow();
+	
 	thread t1(&Game::menu, game);
+
 	t1.join();
 	
 	return 0;
